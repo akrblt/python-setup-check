7f32d5cc370364e20e91f1f512ef27e54c0e334e
akrblt <a.karabulut070@gmail.com>
2024-11-22 10:54:38 +0100

    refactor: externalize ask user input fontion

--- src/main.py
+++ src/main.py
@@ -1,28 +1,12 @@
 operator=None
 operand1=None
 operand2=None
-
+      #Get the operator from the user
 
 def main():
-    ask_user_input()
-    result=calculate(operand1,operator,operand2)
-    display_result(result)
+    ask_user_input();
 
-
-
-
-def ask_user_input():
-    global operand1,operator,operand2
-    # Get first operand from the user
-    operand1 = float(input("Enter the first operand: "))
-
-    # Get the operator from the user
-    operator = input("Enter an operator (+, -, *, /): ")
-
-    # Get second operand from the user
-    operand2 = float(input("Enter the second operand: "))
-
-def calculate(operand1,operator,operand2):
+    #Perform the operation based on the operator
     # Perform the operation based on the operator
     if operator == '+':
         result = operand1 + operand2
@@ -38,10 +22,25 @@ def calculate(operand1,operator,operand2):
     else:
         print("Invalid operator.")
         return
-
-# Print the result
-def display_result(result):
     print("Result:", result)
+
+
+
+
+
+def ask_user_input():
+    global operand1,operator,operand2
+    # Get first operand from the user
+    operand1 = float(input("Enter the first operand: "))
+
+    # Get the operator from the user
+    operator = input("Enter an operator (+, -, *, /): ")
+
+    # Get second operand from the user
+    operand2 = float(input("Enter the second operand: "))
+
+
+
 
 
 # Call the main function to run the program

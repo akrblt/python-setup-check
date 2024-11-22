ab3c9dc175a2d0066322f94352ac01ca8b297048
akrblt <a.karabulut070@gmail.com>
2024-11-22 12:07:30 +0100

    externaliser la fonction qui réalise le calcul

--- src/main.py
+++ src/main.py
@@ -1,12 +1,29 @@
 operator=None
 operand1=None
 operand2=None
+result = None
       #Get the operator from the user
 
 def main():
-    ask_user_input();
+    ask_user_input()
+    global result
+    result = calculate(operand1,operator,operand2)
+    #print the result
+    print("Result:", result)
 
+def ask_user_input():
+        global operand1, operator, operand2
+        # Get first operand from the user
+        operand1 = float(input("Enter the first operand: "))
+
+        # Get the operator from the user
+        operator = input("Enter an operator (+, -, *, /): ")
+
+        # Get second operand from the user
+        operand2 = float(input("Enter the second operand: "))
+def calculate(op1,ope,op2):
     #Perform the operation based on the operator
+    global result
     match operator:
         case '+':
             result = operand1 + operand2
@@ -21,20 +38,11 @@ def main():
             result = operand1 / operand2
         case _:
             print("Invalid operator.")
-            return
-    print("Result:", result)
+            return result
 
 
-def ask_user_input():
-    global operand1,operator,operand2
-    # Get first operand from the user
-    operand1 = float(input("Enter the first operand: "))
 
-    # Get the operator from the user
-    operator = input("Enter an operator (+, -, *, /): ")
 
-    # Get second operand from the user
-    operand2 = float(input("Enter the second operand: "))
 
 # Call the main function to run the program
 main()
